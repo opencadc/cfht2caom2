6e7fbd9ccea6efca30ed1943b63752aa789e47c1
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-07-06 09:43:57 -0700

    s2649 - run the data energy visitor during shakeout.

--- cfht2caom2/composable.py
+++ cfht2caom2/composable.py
@@ -78,7 +78,8 @@ from cfht2caom2 import espadons_energy_augmentation, preview_augmentation
 
 
 meta_visitors = [cleanup_augmentation]
-data_visitors = [espadons_energy_augmentation, preview_augmentation]
+# data_visitors = [espadons_energy_augmentation, preview_augmentation]
+data_visitors = [espadons_energy_augmentation]
 
 CFHT_BOOKMARK = 'cfht_timestamp'
 
