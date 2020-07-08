8cc1b9c573fbb17409cf67a09b1500993f33057d
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-07-08 13:44:43 -0700

    s2650 - scripts to run the container jobs.

---
+++ scripts/cfht_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COLLECTION="cfht"
+IMAGE="bucket.canfar.net/${COLLECTION}2caom2"
+
+echo "Get a proxy certificate"
+cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
+
+echo "Get image ${IMAGE}"
+docker pull ${IMAGE} || exit $?
+
+echo "Run image ${IMAGE}"
+docker run --rm --name ${COLLECTION}_run -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run || exit $?
+
+date
+exit 0
