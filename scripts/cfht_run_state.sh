15eacc78638b777306242b2808d12886164abfbd
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-08-20 15:45:18 -0700

    Update cfht_run_state.sh

--- scripts/cfht_run_state.sh
+++ scripts/cfht_run_state.sh
@@ -7,7 +7,7 @@ echo "Get a proxy certificate"
 cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
 
 echo "Get image ${IMAGE}"
-docker pull ${IMAGE} || exit $?
+docker pull ${IMAGE}
 
 echo "Run image ${IMAGE}"
 docker run --rm --name ${COLLECTION}_run -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run_state || exit $?
