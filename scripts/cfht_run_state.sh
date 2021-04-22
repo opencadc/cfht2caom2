25c7d985c8958d513419e3d8db631e340af2b20b
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-04-22 16:38:58 -0700

    CADC-9337 - add --init flag (#22)
    * Add test files for other tests.
    
    * CADC-9337 - remove ftputil dependency.
    
    * CADC-9337 - integration testing with caom2pipe addition of store_newer_files_only flag.
    
    * CADC-9337 - improve test case post-execution checking.
    
    * CADC-9337 - documentation.
    
    * CADC-9337 - just fixing a WIRCam edge case.
    
    * CADC-9337 - WIRCam edge case.
    
    * CADC-9337 - WIRCam 'g' file edge case.
    
    * CADC-9337 - CFHT edge cases.
    
    * CADC-9337 - add 'b', 'd', 'f', 'x' suffixes to those that can possibly be cleaned up.
    
    * CADC-9337 - CFHT edge case for GAIADR values.
    
    * CADC-9337 - add --init flag, use the opencadc version of the Docker image by default.

--- scripts/cfht_run_state.sh
+++ scripts/cfht_run_state.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COLLECTION="cfht"
-IMAGE="bucket.canfar.net/${COLLECTION}2caom2"
+IMAGE="opencadc/${COLLECTION}2caom2"
 
 echo "Get a proxy certificate"
 cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
@@ -10,7 +10,7 @@ echo "Get image ${IMAGE}"
 docker pull ${IMAGE}
 
 echo "Run image ${IMAGE}"
-docker run --rm --name ${COLLECTION}_run -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run_state || exit $?
+docker run --init --rm --name ${COLLECTION}_run -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run_state || exit $?
 
 date
 exit 0
