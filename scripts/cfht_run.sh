68748c235362a217c60abfe4090bdf86c5c55aec
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-04-22 16:21:53 -0700

    CADC-9337 - add --init flag, use the opencadc version of the Docker image by default.

--- scripts/cfht_run.sh
+++ scripts/cfht_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COLLECTION="cfht"
-IMAGE="bucket.canfar.net/${COLLECTION}2caom2"
+IMAGE="opencadc/${COLLECTION}2caom2"
 
 echo "Get a proxy certificate"
 cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
@@ -10,7 +10,7 @@ echo "Get image ${IMAGE}"
 docker pull ${IMAGE}
 
 echo "Run image ${IMAGE}"
-docker run --rm -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run || exit $?
+docker run --init --rm -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run || exit $?
 
 date
 exit 0
