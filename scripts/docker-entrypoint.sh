7d049c829e5c63a027946470f06e848ae43503fd
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-12-10 14:10:05 -0800

    CADC-10534 - change the location of the yml files, and what gets copied durig the Python install.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 if [[ ! -e ${PWD}/cache.yml ]]
 then
-  cp /usr/local/bin/cache.yml ${PWD}
+  cp /usr/local/.config/cache.yml ${PWD}
 fi
 
 if [[ ! -e ${PWD}/config.yml ]]
 then
-  cp /usr/local/bin/config.yml ${PWD}
+  cp /usr/local/.config/config.yml ${PWD}
 fi
 
 if [[ ! -e ${PWD}/state.yml ]]; then
